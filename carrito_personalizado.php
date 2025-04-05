132c465daf0a2c55caefb6b0b20db26c3a7ac613
oarias93 <eduardo93-12@hotmail.com>
2025-04-04 19:58:34 -0500

    Implementacion del PHP que muestra la data del carrito por medio de un shortcode

--- carrito_personalizado.php
+++ carrito_personalizado.php
@@ -4,4 +4,39 @@ Plugin Name: Carrito personalizado para woocomerce
 Description: Un plugin para simular un carrito de compras en WooCommerce.
 Author: Oscar Arias
 */
-<?php
+
+
+// Función que muestra el carrito en el frontend
+function mostrar_carrito() {
+
+
+    $cart = WC()->cart->get_cart();
+    $subtotal = WC()->cart->get_cart_total();
+    
+    ob_start(); ?>
+    
+    <div class="widget-carrito">
+        <h2>Carrito Personalizado</h2>
+        <ul>
+            <?php foreach ($cart as $item) : ?>
+                <li>
+                    <?php echo $item['data']->get_name(); ?> - 
+                    <?php echo wc_price($item['line_total']); ?>
+                    <form method="POST" action="">
+                        <input type="number" name="cantidad" value="<?php echo $item['quantity']; ?>" />
+                        <button type="submit">Actualizar</button>
+                    </form>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <p>Subtotal: <?php echo $subtotal; ?></p>
+    </div>
+    
+    <?php
+    return ob_get_clean();
+}
+
+// Hook para agregar el carrito al frontend
+add_shortcode('mostrar_carrito', 'mostrar_carrito');
+?>
+
