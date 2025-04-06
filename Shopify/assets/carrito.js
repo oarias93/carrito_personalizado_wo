048a181d2c0a077a601e3f9974889476def3eea4
oarias93 <eduardo93-12@hotmail.com>
2025-04-06 17:35:38 -0500

    Ajustes liquid JS

---
+++ Shopify/assets/carrito.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Toggle del carrito
+    document.querySelector('.toggle-button').addEventListener('click', function() {
+      document.getElementById('widgetCarrito').classList.toggle('show');
+    });
+  
+});
---
+++ Shopify/assets/dist/carrito.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Toggle del carrito
+  document.querySelector('.toggle-button').addEventListener('click', function () {
+    document.getElementById('widgetCarrito').classList.toggle('show');
+  });
+});
